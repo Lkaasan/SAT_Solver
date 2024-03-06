35672bb949523131600f834711712c067eff91a9
Lukas Kaas Andesen <49850026+Lkaasan@users.noreply.github.com>
2024-03-06 19:11:22 +0000

    Started Coding

---
+++ Code/sat_solver.py
@@ -0,0 +1,9 @@
+class Literal:
+    
+    def __init__(self, name, negated):
+        self.name = name
+        self.negated = negated
+        self.occurance = 1
+        
+    def getOccurace(occurance):
+        return self.occurance
